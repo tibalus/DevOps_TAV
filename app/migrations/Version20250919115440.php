01b14a59dd8240b8d8f5b6a30be67143f7a410f8
ValentinDuval <vduval457@gmail.com>
2025-11-30 21:47:59 +0100

    La mort ne serait qu'une douce délivrance

--- app/migrations/Version20250919115440.php
+++ app/migrations/Version20250919115440.php
@@ -20,12 +20,15 @@ final class Version20250919115440 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE user (id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\', fullname VARCHAR(255) NOT NULL, study_level VARCHAR(255) NOT NULL, age INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $sm = $this->connection->createSchemaManager();
+        if (!$sm->tablesExist(['user'])) {
+            $this->addSql('CREATE TABLE user (id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\', fullname VARCHAR(255) NOT NULL, study_level VARCHAR(255) NOT NULL, age INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        }
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE IF EXISTS user');
     }
 }
